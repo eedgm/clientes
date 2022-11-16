2bdaab9f8bbf1eed5af5a179fae7a3389c69d7f8
eedgmnurtech <edegracia@nurtech.org>
2022-11-16 13:09:11 -0500

    Adjust in gantt chart aspect

--- resources/views/app/proposals/gantt.blade.php
+++ resources/views/app/proposals/gantt.blade.php
@@ -16,18 +16,53 @@
         .gantt_section_time .gantt_time_selects select:nth-child(1) {width: 35px;}
         .gantt_section_time .gantt_time_selects select:nth-child(2) {width: 85px;}
         .gantt_section_time .gantt_time_selects select:nth-child(3) {width: 52px;}
+        /* common styles for overriding borders/progress color */
+        .gantt_task_line{ border-color: rgba(0, 0, 0, 0.25); }
+        .gantt_task_line .gantt_task_progress { background-color: rgba(0, 0, 0, 0.25); }
+        .gantt_task_line.high { background-color: #f40303; }
+        .gantt_task_line.high .gantt_task_content { color: #fff; }
+        .gantt_task_line.medium { background-color: #378122; }
+        .gantt_task_line.medium .gantt_task_content { color: #fff; }
+        .gantt_task_line.low { background-color: #e157de; }
+        .gantt_task_line.low .gantt_task_content { color: #fff; }
     </style>
 
     <div id="gantt_here" class="w-screen h-screen"></div>
     <script type="text/javascript">
+        var isMobile = {
+            Android: function() {
+                return navigator.userAgent.match(/Android/i);
+            },
+            BlackBerry: function() {
+                return navigator.userAgent.match(/BlackBerry/i);
+            },
+            iOS: function() {
+                return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+            },
+            Opera: function() {
+                return navigator.userAgent.match(/Opera Mini/i);
+            },
+            Windows: function() {
+                return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+            },
+            any: function() {
+                return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+            }
+        };
+
+        let width = 300;
+        if (isMobile.any()) {
+            width = 150;
+        }
+
         gantt.config.date_format = "%Y-%m-%d %H:%i:%s";
         gantt.config.work_time = true;  // removes non-working time from calculations
         gantt.config.skip_off_time = true;    // hides non-working time in the chart
 
         gantt.config.columns = [
-            {name:"text",       label:"Task name",  width:"300", tree:true },
-            {name:"start_date", label:"Start time", align:"center" },
-            {name:"hours",      label:"Hours",   align:"center" },
+            {name:"text",       label:"Task name",  width:width, tree:true, resize:true},
+            {name:"start_date", label:"Start time", align:"center", resize:true},
+            {name:"hours",      label:"Hours",   align:"center", resize:true},
             {name:"add",        label:"",           width:44 }
         ];
 
@@ -53,14 +88,51 @@
 
         gantt.config.lightbox.sections = [
             {name:"description", height:50, map_to:"text", type:"textarea", focus:true},
-            {name:"priority_id",    height:34, map_to:"priority_id", type:"select", options:opts, default_value: 1},
+            {name:"priority_id", height:34, map_to:"priority_id", type:"select", options:opts, default_value: 1},
             {name:"hours",       height:50, map_to:"hours", type:"textarea"},
             {name:"time",        height:35, map_to:"auto", type:"duration"},
         ];
 
+        gantt.config.order_branch = true;// order tasks only inside a branch
+        gantt.config.order_branch_free = true;
+
+
         gantt.init("gantt_here");
 
+
+        var drag_id = null;
+        gantt.attachEvent("onRowDragStart", function(id, target, e) {
+            drag_id = id;
+            return true;
+        });
+        gantt.attachEvent("onRowDragEnd", function(id, target) {
+            drag_id = null;
+            gantt.render();
+        });
+
+        gantt.templates.grid_row_class = function(start, end, task){
+            if(drag_id && task.id != drag_id){
+                if(task.$level != gantt.getTask(drag_id).$level)
+                    return "cant-drop";
+                }
+            return "";
+        };
+
         gantt.load('/proposal/tasks/{{ $proposal->id }}')
+
+        gantt.templates.task_class  = function(start, end, task){
+            switch (task.priority_id){
+                case 1:
+                    return "high";
+                    break;
+                case 2:
+                    return "medium";
+                    break;
+                case 3:
+                    return "low";
+                    break;
+            }
+        };
 
         var dp = gantt.createDataProcessor(function(entity, action, data, id) {
             data._token = "{{ csrf_token() }}"
